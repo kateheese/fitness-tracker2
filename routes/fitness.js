97e7379a6c8f5ebb423911ae6477f6509a325236
kateheese <kateheese@gmail.com>
2015-10-30 15:10:03 -0600

    display food and exercise for each day

--- routes/fitness.js
+++ routes/fitness.js
@@ -23,8 +23,31 @@ router.post('/:id/new-day', function(req, res) {
 
 router.get('/:id/days/:dayId', function(req, res) {
   lookups.findUser(req.params.id).then(function(user) {
+    var calories = lookups.calculateCalories(user.weight,user.feet,user.inches,user.age,user.sex,user.activity,user.goal);
     lookups.findDay(req.params.dayId).then(function(day) {
-      res.render('fitness/day', { title: day.date, user: user, oauthUser: req.user, day: day });
+      lookups.findFood(day._id).then(function(food) {
+        var foodTotal = 0;
+        food.forEach(function(food) {
+          foodTotal += Number(food.calories);
+        })
+        lookups.findExercise(day._id).then(function(exercise) {
+          var exerciseTotal = 0;
+          exercise.forEach(function(workout) {
+            exerciseTotal += Number(workout.calories);
+          })
+          res.render('fitness/day', {title: day.date,
+            oauthUser: req.user,
+            user: user,
+            day: day,
+            food: food,
+            exercise: exercise,
+            foodTotal: foodTotal,
+            exerciseTotal: exerciseTotal,
+            calories: calories,
+            total: (calories-foodTotal+exerciseTotal),
+            max: (calories+exerciseTotal) });
+        })
+      })
     })
   })
 });
@@ -32,6 +55,34 @@ router.get('/:id/days/:dayId', function(req, res) {
 router.post('/:id/days/:dayId/delete', function(req, res) {
   lookups.deleteDay(req.params.dayId).then(function() {
     res.redirect('/fitness/' + req.params.id);
+  })
+});
+
+router.post('/:id/days/:dayId/add-food', function(req, res) {
+  lookups.findDay(req.params.dayId).then(function(day) {
+    lookups.addFood(day._id, req.body.food, req.body.calories).then(function() {
+      res.redirect('/fitness/' + req.params.id + '/days/' + req.params.dayId);
+    })
+  })
+});
+
+router.post('/:id/days/:dayId/food/:foodId/delete', function(req, res) {
+  lookups.deleteFood(req.params.foodId).then(function() {
+    res.redirect('/fitness/' + req.params.id + '/days/' + req.params.dayId);
+  })
+});
+
+router.post('/:id/days/:dayId/add-exercise', function(req, res) {
+  lookups.findDay(req.params.dayId).then(function(day) {
+    lookups.addExercise(day._id, req.body.exercise, req.body.calories).then(function() {
+      res.redirect('/fitness/' + req.params.id + '/days/' + req.params.dayId);
+    })
+  })
+});
+
+router.post('/:id/days/:dayId/exercise/:exerciseId/delete', function(req, res) {
+  lookups.deleteExercise(req.params.exerciseId).then(function() {
+    res.redirect('/fitness/' + req.params.id + '/days/' + req.params.dayId);
   })
 })
 
